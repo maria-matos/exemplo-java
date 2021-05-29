c4a2b1c43f2a7b3c23e2c74396200c8e2457bcc3
maria-matos <79876268+maria-matos@users.noreply.github.com>
2021-05-29 13:33:56 -0300

    Editando javaScript

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const anoNascimento= prompt ('Digite aqui o seu ano de nascimento')
 
 const nome= prompt ('Digite aqui o seu nome')
 
-alert (`Olá, seu nome é ${nome} e você tem ${anoAtual - anoNascimento} anos `)
+alert (`Olá, seu nome é ${nome} e você tem ou terá ${anoAtual - anoNascimento} anos `)
 
